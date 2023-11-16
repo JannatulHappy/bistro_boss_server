269a09c87c00ff1c161703437536dc0b26062877
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-16 18:58:22 +0600

    delete cart item of specific user

--- index.js
+++ index.js
@@ -75,31 +75,35 @@ async function run() {
     const menusCollection = client.db("bistro_boss").collection("menu");
     const reviewsCollection = client.db("bistro_boss").collection("reviews");
     const cartCollection = client.db("bistro_boss").collection("carts");
-  
- 
-     app.get("/menu", async (req, res) => {
-       const result = await menusCollection.find().toArray();
-       res.send(result);
-     });
 
-     app.get("/reviews", async (req, res) => {
-       const result = await reviewsCollection.find().toArray();
-       res.send(result);
-     });
+    app.get("/menu", async (req, res) => {
+      const result = await menusCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
 
     //  carts collection
     app.get("/carts", async (req, res) => {
-      const email = req.query.email
-      const query={email:email}
-      const result = await cartCollection.find(query).toArray()
-      res.send(result)
-    })
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post("/carts", async (req, res) => {
-      const cartItem = req.body
-      const result = await cartCollection.insertOne(cartItem)
-      res.send(result)
-    })
-
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
